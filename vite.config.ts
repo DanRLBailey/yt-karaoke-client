8ed39e14340638deb25c947112d5c6d45fef5476
DanRLBailey <danbailey.813@gmail.com>
2026-02-22 22:43:24 +0000

    removed unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig, loadEnv, type ConfigEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }: ConfigEnv) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 4173,
     allowedHosts: true
-  },
+  }
 })
-}
