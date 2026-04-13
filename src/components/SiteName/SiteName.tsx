4dc4ee5c665e7edfea7128059c58c8fa28fe7a72
Daniel Bailey <41433945+DanRLBailey@users.noreply.github.com>
2026-04-13 20:37:22 +0100

    added link back to search page

--- src/components/SiteName/SiteName.tsx
+++ src/components/SiteName/SiteName.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import styles from "./SiteName.module.scss";
 import { siteName } from "../../utils/SiteInfo";
+import { useNavigate } from "react-router";
 
 interface SiteNameProps {
   className?: string;
@@ -8,8 +9,13 @@ interface SiteNameProps {
 }
 
 const SiteName = ({ className, size = "md" }: SiteNameProps) => {
+  const navigate = useNavigate();
+
   return (
-    <span className={clsx(styles.siteName, styles[size], className)}>
+    <span
+      className={clsx(styles.siteName, styles[size], className)}
+      onClick={() => navigate("/search")}
+    >
       {siteName()}
     </span>
   );
